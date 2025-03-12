20d911a432a0f91623fa4e5727dad8f33668be1f
Yevhen <apps_1@ukr.net>
2025-03-12 16:33:38 +0200

    fix: correct display close icon in gift card

---
+++ script.js
@@ -0,0 +1,181 @@
+//// burger menu
+const openMenu = document.querySelector('.brand_burger_menu');
+const bodyMenu = document.querySelector('.brand_nav_menu');
+const navMenu = document.querySelectorAll('#nav-menu');
+
+navMenu.forEach(e => {
+    e.addEventListener('click', () => {
+        openMenu.classList.remove('active');
+        bodyMenu.classList.remove('active');
+        document.querySelector('body').classList.remove("hidden");
+    })
+});
+
+openMenu.addEventListener('click', () => {
+    if(openMenu.classList.contains('active') || bodyMenu.classList.contains('active')) {
+        openMenu.classList.remove('active');
+        bodyMenu.classList.remove('active');
+        document.querySelector('body').classList.remove("hidden");
+    }else{
+        openMenu.classList.add('active');
+        bodyMenu.classList.add('active');
+        document.querySelector('body').classList.add("hidden");
+    }
+})
+
+//// slider
+const slides = document.querySelectorAll(".slide_item");
+const previousBut = document.querySelector(".previous_slide");
+const nextBut = document.querySelector(".next_slide");
+/// set up current slide number
+let currentPosition = 0;
+let maxSlides = slides.length;
+let offset = 299;
+/// check screen width
+const deviceScreenWidth = screen.width;
+deviceScreenWidth <= 768 ? maxSlides += 2 : maxSlides;
+///change style button if it's inactve/active
+function updateButtons() {
+    currentPosition === 0 ? previousBut.classList.remove('active_btn') : previousBut.classList.add('active_btn');
+    currentPosition === maxSlides ? nextBut.classList.remove('active_btn') : nextBut.classList.add('active_btn');
+}
+////show slides
+function showNextSlide() {
+    if(currentPosition < maxSlides) {
+        currentPosition++;
+        document.querySelector(".slider_body").style.transform = `translateX(-${currentPosition * offset}px)`;
+    }
+    updateButtons();
+}
+
+function showPreviousSlide() {
+    if(currentPosition > 0) {
+        currentPosition--;
+        document.querySelector(".slider_body").style.transform = `translateX(-${currentPosition * offset}px)`;
+    }
+    updateButtons();
+}
+
+previousBut.addEventListener('click', showPreviousSlide);
+nextBut.addEventListener('click', showNextSlide);
+
+///gifts card popup
+async function getGiftData() {
+    const res = await fetch('./gifts.json');
+    return res.json();
+}
+
+const outputCard = document.querySelectorAll('.gifts_list_item');
+const modalImg = document.querySelector(".modal_img");
+
+async function showGifts() {
+    let giftData = await getGiftData();
+    const sortedGiftData = giftData.sort(() => Math.random() - 0.5);
+    
+    for (let i = 0; i < outputCard.length; i++) {
+        const output = document.createElement('div');
+        output.classList.add('item_description');
+        const h3 = document.createElement('h3');
+        const h4 = document.createElement('h4');
+        h3.textContent = sortedGiftData[i].name;
+        h4.textContent = sortedGiftData[i].category;
+
+        sortedGiftData[i].category === "For Health" ? h4.classList.add('purple') : null;
+        sortedGiftData[i].category === "For Harmony" ? h4.classList.add('green') : null; 
+        sortedGiftData[i].category === "For Work" ? h4.classList.add('blue') : null; 
+    
+        output.appendChild(h4);
+        output.appendChild(h3);
+
+        outputCard[i].append(output);
+        
+    }
+    // showGiftsCard();
+    outputCard.forEach(card => {
+        card.addEventListener('click', () => {
+            const cardImgSrc = card.querySelector('.gifts_list_item .item_photo img').getAttribute('src');
+            const currentCardData = sortedGiftData.find(item => item.name === card.querySelector('h3').textContent);
+            // console.log(currentCard);
+            showPopupContent(currentCardData, cardImgSrc);
+            swichPopup();
+        });
+    })
+}
+
+showGifts();
+
+function swichPopup() {
+    document.querySelector(".popup").classList.add("open");
+    document.querySelector('body').classList.add("hidden");
+    document.querySelector(".close_modal").addEventListener('click', () => {
+        document.querySelector(".popup").classList.remove("open");
+        document.querySelector('body').classList.remove("hidden");
+    })
+};
+
+
+function showPopupContent(currentCard, cardImgSrc) {
+    modalImg.innerHTML = '';
+    modalImg.style.backgroundImage = `url("${cardImgSrc}")`;
+
+    document.querySelector(".modal_main_info h3").textContent = currentCard.name;
+    document.querySelector(".modal_main_info h4").textContent = currentCard.category;
+    document.querySelector(".modal_main_info p").textContent = currentCard.description;
+
+    getCategoryStyle(currentCard.category);
+    
+    const superpowersData = document.querySelector('.superpowers');
+    superpowersData.innerHTML = '';
+
+    for(const title in currentCard.superpowers){
+        const outBlock = document.createElement('li');
+        const outTitle = document.createElement('span');
+        const outScore = document.createElement('span');
+        const starContainer = document.createElement("div");
+
+        outTitle.textContent = title;
+        outScore.textContent = currentCard.superpowers[title];
+
+        outBlock.append(outTitle);
+        outBlock.append(outScore);
+        outBlock.append(starContainer);
+        superpowersData.append(outBlock);
+
+        drawStars(currentCard.superpowers[title], starContainer);
+    }
+};
+
+function getCategoryStyle(currentCard) {
+    const categoryBlock = document.querySelector(".modal_main_info h4");
+    categoryBlock.className = '';
+
+    switch (currentCard) {
+        case "For Health":
+          categoryBlock.classList.add('purple');
+          break;
+        case "For Work":
+          categoryBlock.classList.add('blue');
+          break;
+        case "For Harmony":
+          categoryBlock.classList.add('green');
+          break;
+        default:
+          null;
+    }
+}
+
+
+function drawStars(number, starContainer) {
+    let calculateNumber = number / 100;
+    const maxStar = 5;
+    starContainer.classList.add('star_block');
+
+    for (let i = 0; i < maxStar; i++) {
+        const star = document.createElement('img');
+        star.src = './img/star.png';
+        star.alt = `star${i + 1}`;
+        starContainer.append(star);
+
+        i < calculateNumber ? star.classList.add('star') : star.classList.add('star_opasity');
+    }
+}
